13667c63e1e5bbb4c37c43675c03d2206b9c4c67
StickeMan <2005040@utrivieramaya.edu.mx>
2022-09-19 09:06:00 -0500

    second commit

--- src/common/api/user/dto/createUser.dto.ts
+++ src/common/api/user/dto/createUser.dto.ts
@@ -5,16 +5,16 @@ export class CreateUserDto {
 
   @IsNotEmpty({message: 'El nombre no puede estar vacío'})
   @IsString()
-  name: string;
+  readonly name: string;
 
   @IsNotEmpty({message: 'El username no puede estar vacío'})
   @Length(4, 30)
-  username: string;
+  readonly username: string;
 
   @IsEmail()
-  email: string;
+  readonly email: string;
 
   @IsNotEmpty({message: 'La contraseña del usuario no puede estar vacío'})
   @Length(8)
-  password: string;
+  readonly password: string;
 }
